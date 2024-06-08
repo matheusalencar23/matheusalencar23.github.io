6997affac568b642d2c22da1664560e532bbc315
Matheus Alencar <matheusalencar6942@gmail.com>
2024-06-07 21:35:28 -0300

    construindo seção de projetos com o js

--- app.js
+++ app.js
@@ -26,11 +26,38 @@ function buildServicesSection(experience) {
   $("#services .service-bottom").html(html);
 }
 
+function buildProjectItem(project) {
+  const prefixTag = project.url
+    ? `<a class="project-item" href="${project.url}" target="_blank">`
+    : '<div class="project-item">';
+  const suffixTag = project.url ? "</a>" : "</div>";
+  return `
+  ${prefixTag}
+    <div class="project-info">
+      <h2>${project.name}</h2>
+      <h3>${project.technology}</h3>
+      <p>${project.description}</p>
+    </div>
+    <div class="project-image">
+      <img src="${project.imgUrl}" alt="${project.imgAlt}" />
+    </div>
+  ${suffixTag}`;
+}
+
+function buildProjectSection(projects) {
+  const html = projects.reduce(
+    (html, item) => html + buildProjectItem(item),
+    ""
+  );
+  $("#projects .project-bottom").html(html);
+}
+
 function getData() {
   fetch("data.json", { method: "GET" })
     .then((res) => res.json())
     .then((res) => {
       buildServicesSection(res.experiences);
+      buildProjectSection(res.projects);
     });
 }
 
